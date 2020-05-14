031a1d93f608dc951762430dc61d74da9eb6fe1d
na-win-27 <navin.2019@vitstudent.ac.in>
2020-05-14 18:02:12 +0530

    Updates

---
+++ precache-manifest.81ff25a2de33550a2fd78592ab7ca3b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e7174c38a7f93147436446b4979055f",
+    "url": "/reacttictactoe/index.html"
+  },
+  {
+    "revision": "25c6691f08354f788f67",
+    "url": "/reacttictactoe/static/css/main.44f3bdf8.chunk.css"
+  },
+  {
+    "revision": "086c37047ccdb3baf90a",
+    "url": "/reacttictactoe/static/js/2.c0974068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacttictactoe/static/js/2.c0974068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25c6691f08354f788f67",
+    "url": "/reacttictactoe/static/js/main.c6b32999.chunk.js"
+  },
+  {
+    "revision": "b89926886afce85e336b",
+    "url": "/reacttictactoe/static/js/runtime-main.51d7ecbc.js"
+  }
+]);
